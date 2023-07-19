1cdd9b24de846ad92a596f201b1a957d94b321d6
audreyper <audreyper@protonmail.com>
2023-07-19 15:39:24 -0400

    creating a subclass called 'Book'

--- build-a-library.js
+++ build-a-library.js
@@ -33,4 +33,18 @@ class Media {
     }
   }
   
- 
+  class Book extends Media {
+    constructor(author, title, pages, isCheckedOut, ratings) {
+        super(title, isCheckedOut, ratings)
+        this._author = author 
+        this._pages = pages; 
+    }
+
+    get author() {
+        return this._author
+    }
+
+    get pages() {
+        return this._pages
+    }
+  }
