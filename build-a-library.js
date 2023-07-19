3a053efa7a40c90e2debe38b0c6213f505403316
audreyper <audreyper@protonmail.com>
2023-07-19 16:50:47 -0400

    creating subclass 'Movie'

--- build-a-library.js
+++ build-a-library.js
@@ -48,3 +48,18 @@ class Media {
         return this._pages
     }
   }
+
+  class Movie extends Media {
+    constructor(director, title, runTime, isCheckedOut, ratings) {
+        super(title, isCheckedOut, ratings) 
+        this._director = director; 
+        this._runTime = runTime; 
+    }
+    get director() {
+        return this._director;
+    }
+
+    get runTime() {
+        return this._runTime; 
+    }
+  }
